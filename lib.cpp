aef85128196d83f92ebf1dc849218c9dc805f3d0
AndreaDotta <117829484+S5475373@users.noreply.github.com>
2022-12-08 17:13:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+bool carattere(char &c){
+    if (c >= 65 && c <= 90) {
+        c += 32;
+        return true;
+    }
+    else if (c >= 97 && c <= 122) {
+        c -= 32;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
