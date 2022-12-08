e43a8d59640a25e8ce9ef5e9233d32dad813d38e
AndreaDotta <117829484+S5475373@users.noreply.github.com>
2022-12-08 17:15:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char c;
+  cin >> c;
+  if (carattere(c) == true) {
+      cout << c;
+  }
+  else {
+      cout << "errore";
+  }
   return 0;
 }
